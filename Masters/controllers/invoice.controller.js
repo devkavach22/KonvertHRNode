0d623797bf3cae3620728c1f73376404fa108a6d
devkavach22 <developer@kavachglobal.com>
2026-03-19 11:09:11 +0530

    change the url of the

--- Masters/controllers/invoice.controller.js
+++ Masters/controllers/invoice.controller.js
@@ -555,7 +555,7 @@ module.exports = {
         if (email) {
           const subject =
             "Subscription Activated - Kavach Global Konnects Pvt Ltd";
-          const loginUrl = "https://saas.konverthr.com/KHR-plan-activation";
+          const loginUrl = "https://odoosaas.konverthr.com/KHR-plan-activation";
 
           const htmlContent = `
 <!DOCTYPE html>
